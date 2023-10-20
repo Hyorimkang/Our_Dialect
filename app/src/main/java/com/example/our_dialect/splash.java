fb80286b2e2557a9c38cef1eeb5ba56819d66914
강효림 <103922285+Hyorimkang@users.noreply.github.com>
2023-10-20 23:06:44 +0900

    :sparkles: 처음 로딩화면 만들기

---
+++ app/src/main/java/com/example/our_dialect/splash.java
@@ -0,0 +1,34 @@
+package com.example.our_dialect;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //타이틀 바 없애기
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        Loadingstart();
+    }
+
+    private void Loadingstart(){
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+}
